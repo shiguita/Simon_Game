4d8e78bff52c530c12a959406ec264e91982bdb9
santiago.higuita <shiguita2@alumno.uned.es>
2023-06-23 14:08:26 +0100

    Added sounds and comments for JS

--- index.js
+++ index.js
@@ -2,30 +2,53 @@
 const colors = ["green", "red", "yellow", "blue"];
 let highscore = 0;
 
+const greenAudio = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
+);
+const redAudio = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"
+);
+const yellowAudio = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"
+);
+const blueAudio = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"
+);
+
 const msg = document.getElementById("msg");
 const background = document.querySelector("body");
 const points = document.getElementById("score");
 
 class GameAlgorithm {
   constructor() {
+    /**
+     * We instantiate:
+     * - the sequence of colors
+     * - the index for playing the sequence and checking player inputs
+     * - score for the current instance
+     * - game states
+     */
     this.sequence = [];
     this.indexOfSeq = 0;
     this.score = 0;
     this.isGameAlive = false;
     this.seqIsPlaying = false;
 
+    // Event binding
     this.buttons = document.querySelectorAll(".button");
     this.buttons.forEach((button) => {
-      button.addEventListener("click", this.buttonHandler.bind(this));
+      button.addEventListener("click", (e) => this.buttonHandler(e));
     });
   }
 
+  // Reseting the game parameters
   resetGame() {
     this.sequence = [];
     this.indexOfSeq = 0;
     this.score = 0;
   }
 
+  // Adding new color to sequence
   addToSequence() {
     this.seqIsPlaying = true;
     const newColor = colors[Math.floor(Math.random() * colors.length)];
@@ -36,6 +59,7 @@ class GameAlgorithm {
     }, 300);
   }
 
+  // Show the sequence of colors
   playSequence() {
     let myInterval = setInterval(() => {
       this.buttonAnimation(
@@ -53,6 +77,7 @@ class GameAlgorithm {
     }, 800);
   }
 
+  // Handling of button clicks
   buttonHandler(event) {
     if (this.isGameAlive && !this.seqIsPlaying) {
       const clickedColor = event.target.classList[1];
@@ -70,6 +95,7 @@ class GameAlgorithm {
     }
   }
 
+  // Update the score and check for highscore
   updateScore() {
     this.score++;
     points.textContent = this.score;
@@ -79,11 +105,13 @@ class GameAlgorithm {
     }
   }
 
+  // Initiate game sequence
   beginGame() {
     this.isGameAlive = true;
     this.addToSequence();
   }
 
+  // Game ends when failing
   endGame() {
     msg.textContent = "TRY AGAIN?";
     background.classList.add("errorBG");
@@ -91,19 +119,38 @@ class GameAlgorithm {
     this.resetGame();
   }
 
+  // Animation and sound for the buttons
   buttonAnimation(button) {
     button.classList.add(button.classList[1] + "Glow");
+    switch (button.classList[1]) {
+      case "green":
+        greenAudio.play();
+        break;
+      case "red":
+        redAudio.play();
+        break;
+      case "yellow":
+        yellowAudio.play();
+        break;
+      case "blue":
+        blueAudio.play();
+        break;
+
+      default:
+        break;
+    }
     setTimeout(() => {
       button.classList.remove(button.classList[1] + "Glow");
     }, 300);
   }
 }
 
+// Event listener for starting a game
 const simonGame = new GameAlgorithm();
 document.querySelector(".circle").addEventListener("click", () => {
   if (!simonGame.isGameAlive) {
-    simonGame.beginGame();
     background.classList.remove("errorBG");
     points.textContent = 0;
+    simonGame.beginGame();
   }
 });
